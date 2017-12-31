dba5cc16a4febf0e156a8d71fdf175ef0e99fcda
Alex Popescu <the.mindstorm@gmail.com>
2017-12-31 01:42:21 -0800

    Make all assertions pass
    Tests are still failing as exercising directly the `testing.T` API
    changes the internal state of tests.

--- testing_test.go
+++ testing_test.go
@@ -6,36 +6,44 @@ import (
 	"testing"
 )
 
-func TestSkip(t *testing.T) {
+func TestTestingSkip(t *testing.T) {
 	t.Skip(t.Name())
 	assert.Fail(t, "Skip should never get to this step")
 }
-func TestSkipf(t *testing.T) {
+
+func TestTestingSkipf(t *testing.T) {
 	t.Skipf("t.Skipf %s", t.Name())
 	assert.Fail(t, "Skipf should never get to this step")
 }
 
-func TestError(t *testing.T) {
+// Test will fail cause of `t.Error` and not due to the assertion
+func TestTestingError(t *testing.T) {
 	t.Error("t.Error")
-	t.Logf("test %s passed %t", t.Name(), !t.Failed())
+	assert.True(t, t.Failed(), "expect %s marked as failed, actual %t", t.Name(), t.Failed())
 }
 
-func TestErrorf(t *testing.T) {
+// Test will fail cause of `t.Error` and not due to the assertion
+func TestTestingErrorf(t *testing.T) {
 	t.Errorf("firstly %s is not yet marked as failed %t", t.Name(), t.Failed())
-	t.Logf("test %s passed %t", t.Name(), !t.Failed())
+	assert.True(t, t.Failed(), "expect %s marked as failed, actual %t", t.Name(), t.Failed())
 }
 
-func TestFail(t *testing.T) {
+func TestTestingFail(t *testing.T) {
 	t.Fail()
-	assert.True(t, t.Failed(), "%s was expected to be marked as failed", t.Name())
+	assert.True(t, t.Failed(), "expect %s marked as failed, actual %t", t.Name(), t.Failed())
 }
 
-func TestFatal(t *testing.T) {
-	t.Fatal("t.Fatal")
+func TestTestingFatal(t *testing.T) {
+	t.Fatal("t.Fatal() on %s", t.Name())
 	assert.Fail(t, "Fatal stops execution right away")
+}
+
+func TestTestingFatalf(t *testing.T) {
+	t.Fatal("t.Fatal on %s", t.Name())
+	assert.Fail(t, "t.Fatal stops execution right away")
 }
 
 func ExampleShowingOutputBasedTesting() {
 	fmt.Println("this is it")
-	// Output: this is not it
+	// Output: this is it
 }
